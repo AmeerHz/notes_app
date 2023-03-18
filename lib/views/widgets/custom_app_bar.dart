31064891bce5a95679f520f2e56b550320f79a4c
AmeerHz <ameer.hamze9@gmail.com>
2023-03-18 14:44:11 -0700

    create note model

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -3,22 +3,23 @@ import 'package:notes_app/views/widgets/custom_app_bar_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key, required this.title, required this.icon});
- final String title;
- final IconData icon;
+  final String title;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Row(
-      children:  [
+      children: [
         Text(
           title,
-          style:const TextStyle(
+          style: const TextStyle(
             fontSize: 32,
           ),
         ),
         const Spacer(),
-        CustomAppBarIcon(icon: icon,),
+        CustomAppBarIcon(
+          icon: icon,
+        ),
       ],
     );
   }
 }
-
